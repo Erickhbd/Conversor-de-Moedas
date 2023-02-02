53ff04eddd5318350b95695c59dd1c352d66b656
Erick Luciano <102949948+Erickhbd@users.noreply.github.com>
2023-02-02 00:35:34 -0300

    atualização na rota do script

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function Converter() {
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseFloat(valor);
+    var valorEmDolar = parseFloat(valor);
   
-    var valorEmReal = valorEmDolarNumerico * 5;
-    console.log(valorEmReal);
+    var valorEmReal = valorEmDolar * 5.07; //em 31:1:2013 as 21:46
   
+    setTimeout(() => {
+      window.location.reload ()
+    }, 5000);
+
     var elementoValorConvertido = document.getElementById("valorConvertido");
     var valorConvertido = "O resultado em real é R$ " + valorEmReal;
     elementoValorConvertido.innerHTML = valorConvertido;
   }
+
+  // DESAFIO
+  // trocar por outras moedas
+  // perguntar e imprimir nome
+  // converter anos luz em metros
